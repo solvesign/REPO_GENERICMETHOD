66479c36bb0715cf351d64c640bfe47185ad3a61
solvesign <solvesign@gmail.com>
2026-02-18 12:32:11 -0600

    added new properties

---
+++ REPO_GENERICMETHOD/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace REPO_GENERICMETHOD.Models
+{
+    public class Order
+    {
+        public int OrderId {  get; set; }
+
+        public int ProductID { get; set; }
+
+        public int Qty { get; set; }
+
+        public int PlayerId {  get; set; }
+
+        ForeignKey
+
+
+
+
+
+
+
+
+    }
+}
---
+++ REPO_GENERICMETHOD/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace REPO_GENERICMETHOD.Models
+{
+    public class Product
+    {
+        [Key]
+       
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId {  get; set; }
+
+        public string Name { get; set; }
+
+        public int Qty { get; set; }
+
+        public double Price { get; set; }
+
+        public double TotalAmount => Qty * Price;
+
+
+        public string Description { get; set; }
+
+
+    }
+}
